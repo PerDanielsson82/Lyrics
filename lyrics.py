944e2cb21d7aa560024049a9eae820e7c804f5e3
Per_D <perd16@hotmail.com>
2026-07-12 19:42:26 +0200

    Remove music player links from lyrics.py
    Removed st_player calls for music links and retained PDF display for lyrics.

--- lyrics.py
+++ lyrics.py
@@ -4,7 +4,6 @@
 #  streamlit run lyrics.py
 
 import streamlit as st
-import st_player 
 
 st.image("pictures/Logo.png", output_format="JPEG")    
 st.title("Album Lyrics", text_alignment="center")
@@ -19,20 +18,15 @@ button3 = st.sidebar.button("*Skydiving*")
 button4 = st.sidebar.button("*In times of sadness*")
 
 if button1:
-    st_player("https://music.youtube.com/watch?v=fv2TtaVT9q8&si=wPA8m0A0GCKYgVa2")
     st.pdf("lyrics/Fridays at Frans.pdf")
 
-
 if button2:
-    st_player("https://music.youtube.com/watch?v=vEiAtr7pqCA&si=q8_HQzu2DiWSOvSs")
     st.pdf("lyrics/Devils dance.pdf")
 
 if button3:
-    st_player("https://music.youtube.com/watch?v=t0TSAopt-lY&si=6tWT2jEMqmHtrpmO")
     st.pdf("lyrics/Skydiving.pdf")
 
 if button4:
-    st_player("https://music.youtube.com/watch?v=jDiZIBp-XG0&si=W5Dbove8c8wqasw3")
     st.pdf("lyrics/In times of sadness.pdf")
 
 
@@ -42,12 +36,9 @@ button5=st.sidebar.button("*Laughter in the night*")
 button6=st.sidebar.button("*At the stationhouse*")
 
 if button5:
-    st_player("https://music.youtube.com/watch?v=Kfv_muHA5LU&si=b8uihi33k8oWD8nc")
     st.pdf("lyrics/Laughter in the night.pdf")
     
-
 if button6:
-    st_player("https://music.youtube.com/watch?v=xgM0CPPAgxY&si=-jAGPFrxRrCwJv8g")
     st.pdf("lyrics/At the stationhouse.pdf")
 
 
